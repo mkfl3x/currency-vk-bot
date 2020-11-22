e5a9ca6165ec6909311497aebebe1a255f4ace7c
Mikhail Kuznetsov <mkfl3x@gmail.com>
2020-11-22 20:54:22 +0300

    attachments and payload handling added

--- src/main/java/com/vkbot/callback/CallbackHandler.java
+++ src/main/java/com/vkbot/callback/CallbackHandler.java
@@ -2,14 +2,43 @@ package com.vkbot.callback;
 
 import com.vk.api.sdk.callback.CallbackApi;
 import com.vk.api.sdk.objects.messages.Message;
+import com.vk.api.sdk.objects.messages.MessageAttachment;
+import com.vkbot.callback.models.Payload;
 import com.vkbot.core.BotActions;
+import com.vkbot.rates.Rates;
+import com.vkbot.utils.GsonMapper;
+
+import java.util.List;
 
 public class CallbackHandler extends CallbackApi {
 
     private final BotActions actions = new BotActions();
 
+    private void attachmentsHandling(int peerId, List<MessageAttachment> attachments) {
+        actions.sendMessage(peerId, "Attachments handling is not supported");
+    }
+
+    private void payloadHandling(int peerId, String payload) {
+        Payload buttonCase = GsonMapper.deserialize(payload, Payload.class);
+        switch (buttonCase.getButtonId()) {
+            case 1:
+                actions.sendMessage(peerId, Rates.getRateInfo("USD"));
+                break;
+            case 2:
+                actions.sendMessage(peerId, Rates.getRateInfo("EUR"));
+                break;
+            default:
+                throw new RuntimeException("Unrecognized button_id was tried to use");
+        }
+    }
+
     @Override
     public void messageNew(Integer groupId, Message message) {
-        actions.sendMessage(356234071, "hello");
+        if (message.getPayload() != null)
+            payloadHandling(message.getPeerId(), message.getPayload());
+        if (message.getAttachments() != null) {
+            attachmentsHandling(message.getPeerId(), message.getAttachments());
+        }
+        actions.sendKeyboard(message.getPeerId());
     }
 }
