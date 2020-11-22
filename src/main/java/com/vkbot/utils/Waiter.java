4de7ca196b08fed756143065dadef805c12beee6
Mikhail Kuznetsov <mkfl3x@gmail.com>
2020-11-22 20:51:51 +0300

    waiter util was added

---
+++ src/main/java/com/vkbot/utils/Waiter.java
@@ -0,0 +1,14 @@
+package com.vkbot.utils;
+
+import java.util.concurrent.TimeUnit;
+
+public class Waiter {
+
+    public static void wait(int minutes) {
+        try {
+            TimeUnit.MINUTES.sleep(minutes);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
