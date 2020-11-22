b7ab49ba05d57e7d967e94ae5dfbd4c2ecea0f60
Mikhail Kuznetsov <mkfl3x@gmail.com>
2020-11-22 20:53:17 +0300

    Currency keyboard sending was added

--- src/main/java/com/vkbot/core/BotActions.java
+++ src/main/java/com/vkbot/core/BotActions.java
@@ -1,13 +1,15 @@
 package com.vkbot.core;
 
-import com.vk.api.sdk.client.AbstractQueryBuilder;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.GroupActor;
-import com.vk.api.sdk.exceptions.ApiException;
-import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.messages.Keyboard;
+import com.vkbot.utils.GsonMapper;
+import com.vkbot.utils.ResourcesLoader;
 
 import java.util.Random;
+
+import static com.vkbot.utils.RequestExecutor.performRequest;
 
 public class BotActions {
 
@@ -18,19 +20,25 @@ public class BotActions {
             PropertiesManager.getProperty("bot.token")
     );
 
-    private <T, R> R executeRequest(AbstractQueryBuilder<T, R> query) {
-        try {
-            return query.execute();
-        } catch (ApiException | ClientException e) {
-            throw new RuntimeException("Request execution was failed", e);
-        }
-    }
-
     public void sendMessage(int peerId, String text) {
-        executeRequest(
+        performRequest(
                 vk.messages().send(group)
                         .peerId(peerId)
                         .message(text)
+                        .randomId(new Random().nextInt())
+        );
+    }
+
+    public void sendKeyboard(int peerId) {
+        Keyboard keyboard = GsonMapper.deserialize(
+                ResourcesLoader.getResource("/keyboard.json"),
+                Keyboard.class
+        );
+        performRequest(
+                vk.messages().send(group)
+                        .peerId(peerId)
+                        .message("")
+                        .keyboard(keyboard)
                         .randomId(new Random().nextInt())
         );
     }
