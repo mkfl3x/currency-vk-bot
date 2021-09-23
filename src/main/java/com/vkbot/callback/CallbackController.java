99850343ac351ef110dd49e8f21c25bcc3aae3aa
Mikhail Kuznetsov <mkfl3x@gmail.com>
2021-09-23 20:33:26 +0300

    primitive logging

--- src/main/java/com/vkbot/callback/CallbackController.java
+++ src/main/java/com/vkbot/callback/CallbackController.java
@@ -3,6 +3,8 @@ package com.vkbot.callback;
 import com.vk.api.sdk.objects.callback.messages.CallbackMessage;
 import com.vkbot.core.PropertiesManager;
 import com.vkbot.utils.GsonMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CallbackController {
 
+    private final Logger logger = LoggerFactory.getLogger(CallbackController.class);
+
     private final CallbackHandler callbackHandler = new CallbackHandler(PropertiesManager.getProperty("bot.confirmation"));
 
     @PostMapping("/callback")
     public String callbackReceive(@RequestBody String message) {
         CallbackMessage callback = GsonMapper.deserialize(message, CallbackMessage.class);
+        logger.info("New event. Type: {}, GroupID: {}", callback.getType(), callback.getGroupId());
         return callbackHandler.parse(callback);
     }
 }
